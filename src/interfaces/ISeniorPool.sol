98f0f99ef6dea088423f1da586fda31f7a399623
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-09-08 16:41:28 +0300

    Updated Interfaces

--- src/interfaces/ISeniorPool.sol
+++ src/interfaces/ISeniorPool.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.19;
 import {ReignConfig} from "../protocol/ReignConfig.sol";
 /**
  * @title Opportunity Pool Interface
- * @author 
+ * @author
  * @notice This contract is used to define the interface for the Senior Pool
  */
- */
+
 interface ISeniorPool {
     function withdrawFromOpportunity(bool _isWriteOff, bytes32 _opportunityId, uint256 _amount) external;
 }
