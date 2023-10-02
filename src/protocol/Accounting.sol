c1714f58dc2a592520f9e2902d6ca306498fe74f
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-10-02 19:23:30 +0300

    Created the accounitng library

---
+++ src/protocol/Accounting.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {DSMath} from "./DSMath.sol";
+import {Constants} from "./Constants.sol";
+import {SafeMathUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+
+library Accounting {}
