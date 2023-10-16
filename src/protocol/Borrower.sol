3d9c8885cb3237667237ad61b40227e8e1aa9cd8
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-10-16 19:08:56 +0300

    Created borrower profile

--- src/protocol/Borrower.sol
+++ src/protocol/Borrower.sol
@@ -11,11 +11,18 @@ contract Borrower is BaseUpgradeablePausable {
 
     using ConfigHelper for ReignConfig;
 
+    mapping(address => string) public borrowerProfile;
+
     function initialize(ReignConfig _reignConfig) external initializer {
         require(address(_reignConfig) != address(0), "Invalid ReignConfig Address");
         reignConfig = ReignConfig(_reignConfig);
         address owner = reignConfig.reignAdminAddress();
         require(owner != address(0), "Invalid Owner Address");
         _BaseUpgradeablePausable_init(owner);
+    }
+
+    function updateBorrowerProfile(string calldata _cid) external {
+        require(bytes(_cid).length != 0, "Invalid CID");
+        borrowerProfile[msg.sender] = _cid;
     }
 }
