04a474b634dc710ad75c69dbc3b874932dae5bd8
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-10-18 19:51:52 +0300

    Updated the code files

--- src/protocol/SeniorPool.sol
+++ src/protocol/SeniorPool.sol
@@ -2,4 +2,14 @@
 
 pragma solidity 0.8.4;
 
-contract SeniorPool {}
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./PauserPausable.sol";
+import "./Constants.sol";
+
+contract SeniorPool is BaseUpgradeable,{
+
+}
