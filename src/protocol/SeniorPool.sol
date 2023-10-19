c81e59e6fdf4c6de07954726cf7bf27854f69b8d
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-10-19 20:08:22 +0300

    Updated the code files

--- src/protocol/SeniorPool.sol
+++ src/protocol/SeniorPool.sol
@@ -16,4 +16,75 @@ import {IOpportunityPool} from "../interfaces/IOpportunityPool.sol";
 import {ConfigOptions} from "../libraries/ConfigOptions.sol";
 import {Constants} from "../Constants.sol";
 
-contract SeniorPool is BaseUpgradeablePausable, ISeniorPool {}
+contract SeniorPool is BaseUpgradeablePausable, ISeniorPool {
+    using SafeMathUpgradeable for uint256;
+
+    ReignConfig private reignConfig;
+
+    using ConfigHelper for ReignConfig;
+
+    IOpportunityManager private opportunityManager;
+
+    struct InvestmentTimestamp {
+        uint256 timestamp;
+        uint256 amount;
+    }
+
+    struct KYC {
+        bool isDocument;
+        bool isLikeness;
+        bool isAddress;
+        bool isAML;
+        bool imageHash;
+        bool result;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    ////                        Mappings                                ////
+    /////////////////////////////////////////////////////////////////////////
+
+    mapping(address => InvestmentTimestamp[]) private s_stakingAmount;
+    mapping(address => uint256) private s_availableToWithdraw;
+    mapping(address => bool) public s_isStaking;
+    mapping(address => uint256) private s_usdcYield;
+    mapping(address => KYC) private s_kycOf;
+
+    /////////////////////////////////////////////////////////////////////////
+    ////                        State Variables                          ////
+    /////////////////////////////////////////////////////////////////////////
+
+    string public s_contractName = "SeniorPool";
+    IERC20 private s_usdcToken;
+    IReignCoin private s_reignToken;
+    uint256 public s_investmentLockInMonths;
+    uint256 public s_seniorPoolBalance;
+    uint256 public s_sharePrice;
+
+    /////////////////////////////////////////////////////////////////////////
+    ////                        Events                                   ////
+    /////////////////////////////////////////////////////////////////////////
+
+    event Stake(address indexed from, uint256 amount);
+    event Unstake(address indexed from, uint256 amount);
+    event YieldWithdraw(address indexed to, uint256 amount);
+
+    /////////////////////////////////////////////////////////////////////////
+    ////                        Functions                                ////
+    /////////////////////////////////////////////////////////////////////////
+
+    function initialize(ReignConfig _reignConfig) public initializer {
+        require(address(_reignConfig) != address(0), "SeniorPool: reignConfig cannot be zero address");
+
+        reignConfig = _reignConfig;
+        address owner = reignConfig.reignAdminAddress();
+        require(owner != address(0), "SeniorPool: reignAdminAddress cannot be zero address");
+
+        opportunityManager = IOpportunityManager(reignConfig.opportunityManagerAddress());
+
+        _BaseUpgradeablePausable_init(owner);
+        s_usdcToken = IERC20(reignConfig.usdcTokenAddress());
+        s_reignToken = IReignCoin(reignConfig.reignTokenAddress());
+        s_investmentLockInMonths = reignConfig.getSeniorPoolInvestmentLockInMonths();
+        s_sharePrice = 0;
+    }
+}
