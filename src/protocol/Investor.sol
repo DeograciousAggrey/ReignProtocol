c0bcf98ac444559a6d0d952d0c721feeeffaf203
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-11-06 15:42:55 +0300

    Finished all the contracts

--- src/protocol/Investor.sol
+++ src/protocol/Investor.sol
@@ -27,6 +27,49 @@ contract Investor is BaseUpgradeablePausable, IInvestor {
     }
 
     function addOpportunity(address _investor, bytes32 _opportunityId) external override {
-        address poolAddress = opportunityManager.getOpportunityPool(_opportunityId);
+        address poolAddress = opportunityManager.getOpportunityPoolAddress(_opportunityId);
+        require(_investor != address(0), "Invalid Address");
+        require(poolAddress != address(0), "Invalid Pool Address");
+        require(msg.sender == poolAddress, "Invalid Caller");
+        require(IOpportunityPool(poolAddress).isStaking(_investor), "Invalid Staker");
+        bool exists = getInvestor(_investor, _opportunityId);
+        if (!exists) {
+            investorToOpportunity[_investor].push(_opportunityId);
+        }
+    }
+
+    function removeOpportunity(address _investor, bytes32 _opportunityId) external override {
+        address poolAddress = opportunityManager.getOpportunityPoolAddress(_opportunityId);
+        require(_investor != address(0), "Invalid Address");
+        require(poolAddress != address(0), "Invalid Pool Address");
+        require(msg.sender == poolAddress, "Invalid Caller");
+        require(IOpportunityPool(poolAddress).isStaking(_investor), "Invalid Staker");
+        bool exists = getInvestor(_investor, _opportunityId);
+        require(exists, "Not Found");
+
+        bytes32[] memory opportunityIds = investorToOpportunity[_investor];
+        for (uint256 i = 0; i < opportunityIds.length; i++) {
+            if (opportunityIds[i] == _opportunityId) {
+                investorToOpportunity[_investor][i] = investorToOpportunity[_investor][opportunityIds.length - 1];
+                delete investorToOpportunity[_investor][opportunityIds.length - 1];
+            }
+        }
+    }
+
+    function getOpportunities(address _investor) external view override returns (bytes32[] memory) {
+        require(_investor != address(0), "Invalid Address");
+        bytes32[] memory opportunities = investorToOpportunity[_investor];
+        return opportunities;
+    }
+
+    function getInvestor(address _investor, bytes32 _opportunityId) public view override returns (bool) {
+        require(_investor != address(0), "Invalid Address");
+        bytes32[] memory opportunities = investorToOpportunity[_investor];
+        for (uint256 i = 0; i < opportunities.length; i++) {
+            if (opportunities[i] == _opportunityId) {
+                return true;
+            }
+        }
+        return false;
     }
 }
